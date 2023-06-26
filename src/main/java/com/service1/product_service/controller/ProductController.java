5a72fb8ffc752f5dd330bf3bd43a9d4bccb63ff6
Sebastiano <RZUSST02B24C351W@studium.unict.it>
2023-06-26 10:23:47 +0200

    BUILD FUNZIONANTE
    testata con postman

--- src/main/java/com/service1/product_service/controller/ProductController.java
+++ src/main/java/com/service1/product_service/controller/ProductController.java
@@ -33,7 +33,7 @@ public class ProductController {
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
 	 public List<ProductResponse> getAllProducts(){
-		productService.getAllProducts();
+		return productService.getAllProducts();
 	}
 
 }
